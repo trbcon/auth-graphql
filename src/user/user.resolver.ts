e5b6196e9687f14e80367c3128f0bdcfbf849b9c
trbcon <pahax.off@gmail.com>
2026-03-19 21:08:21 +0300

    getMe + auth decorators

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,10 +1,20 @@
 import { Query, Resolver } from '@nestjs/graphql';
 import { UserService } from './user.service';
 import { UserModel } from './models/user.model';
+import { Authorization } from 'src/auth/decorators/authorization.decorator';
+import { Authorized } from 'src/auth/decorators/authorized.decorator';
+import type { User } from 'src/generated/prisma/client';
+
 
 @Resolver()
 export class UserResolver {
   constructor(private readonly userService: UserService) {}
+
+  @Authorization()
+  @Query(() => UserModel)
+  getMe(@Authorized() user: User) {
+    return user
+  }
 
   @Query(() => [UserModel])
   getUsers() {
